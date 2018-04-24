629f2bba88975291b643c35b02cb03920e4a397c
aj-5001 <alecjasanovsky@gmail.com>
2018-04-24 16:29:41 -0400

    array resersal

--- TestingCMD/Program.cs
+++ TestingCMD/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestingCMD
+class ReverseArray
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+        int[] arr = new int[10];
+
+        for (int i = 0; i < 10; i++)
         {
+            Console.WriteLine("Enter the array elements[" + i + "]:");
+            arr[i] = int.Parse(Console.ReadLine());
         }
+        //write your logic here
+       
+
+        Array.Reverse(arr);
+ 
+
+
+
+
+          //end 
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.ReadKey();
     }
 }
